32800c3d4668eadbbba3d66bce476274e7f60c72
Jiasheng Wu <jiashenw@usc.edu>
2019-08-30 14:39:39 -0700

    Change SPARQL endpoint

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,6 +1,7 @@
 export const commonsEndpoint = 'https://commons.wikimedia.org/w/api.php'
 export const wikidataEndpoint = 'https://www.wikidata.org/w/api.php'
-export const sparqlEndpoint = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
+// export const sparqlEndpoint = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
+export const sparqlEndpoint = 'http://sitaware.isi.edu:8080/bigdata/namespace/wdq/sparql'
 
 export const MAX_SIMULTANEOUS_API_REQUESTS = 25
 export const MAX_ENTITIES_PER_API_REQUEST = 50
